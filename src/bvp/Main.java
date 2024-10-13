eea44f68c30e318a96768e4e12c16ca4b09ed39c
UnpolishedNoob <banikpritom321@gmail.com>
2024-10-14 04:01:39 +0600

    added starting screen

--- src/bvp/Main.java
+++ src/bvp/Main.java
@@ -2,20 +2,21 @@ package bvp;
 
 import bvp.views.GameBoard;
 import bvp.views.OnStartBoard;
+import bvp.views.Starting;
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameBoard onStartBoard = new GameBoard();
-
         JFrame window = new JFrame();
 
         window.setTitle("Bee V Panda: Adventure in Jungle");
         window.setSize(1000, 700);
 
-        window.add(onStartBoard);
+        Starting startBoard = new Starting(window);
+        window.add(startBoard);
+
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setVisible(true);
     }
